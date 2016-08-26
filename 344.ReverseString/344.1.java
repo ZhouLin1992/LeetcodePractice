df6cbd82beeb00c5a52520b7c90f5ca0935b94e6
ZhouLin1992 <lzhou0922@ufl.edu>
2016-08-26 11:58:57 -0400

    344.ReverseString
    344.1

--- 344.ReverseString/344.1.java
+++ 344.ReverseString/344.1.java
@@ -1,10 +1,10 @@
-public static void main(String args[]){
-	public String reverseString(String s) {
-		if (s == null) 
+public class Solution {
+    public String reverseString(String s) {
+        if (s == null) 
 			return null;
 		if (s == "")
 			return "";
 		StringBuilder str = new StringBuilder(s);
 		return str.reverse().toString();
-	}
+    }
 }
