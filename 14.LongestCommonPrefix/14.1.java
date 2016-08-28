df8ba18b3f16f83e0836f6e4ce8719fbf4e26d13
ZhouLin1992 <lzhou0922@ufl.edu>
2016-08-28 12:19:59 -0400

    14.LongestCommonPrefix
    14.1

---
+++ 14.LongestCommonPrefix/14.1.java
@@ -0,0 +1,10 @@
+public class Solution {
+	public String longestCommonPrefix(String[] strs) {
+		if (strs.length == 0 || strs ==  null) return "";
+		String prev = strs[0];
+		for (int i = 1; i < strs.length; i++) {
+			while (strs[i].indexOf(prev) != 0) 
+				prev = prev.substring(0, prev.length() - 1);
+		}
+		return prev;
+}
