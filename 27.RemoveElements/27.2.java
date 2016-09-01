f13628c3aa6a3060700907dab1f21f4b07948b1e
ZhouLin1992 <lzhou0922@ufl.edu>
2016-08-31 21:10:39 -0400

    27.RemoveElements
    27.1/2

---
+++ 27.RemoveElements/27.2.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+		if (nums == null || nums.length == 0) return 0;
+		int len = nums.length;
+		for (int i = 0; i < len; i++) {
+			while (nums[i] == val && i < len) {
+				nums[i] = nums[--len];
+			}
+		}
+		return len;
+    }
+}
