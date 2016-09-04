8a8f623779e7207c19ed91db65f12a6d20ab274c
ZhouLin1992 <lzhou0922@ufl.edu>
2016-09-04 14:06:42 -0400

    202.HappyNumber
    202.1

---
+++ 202.HappyNumber/202.1.java
@@ -0,0 +1,15 @@
+public class Solution {
+	public boolean isHappy(int n) {
+		if (n <= 0) return false;
+
+		while(n >= 10) {
+			int sum = 0;
+			while(n > 0) {
+				sum += Math.pow(n % 10, 2);
+				n /= 10;
+			}
+			n = sum;
+		}
+		return n == 1 || n == 7;
+	}
+}
