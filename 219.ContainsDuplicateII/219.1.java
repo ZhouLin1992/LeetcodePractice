6b50425b239013c82b09d1cd9b3f04d9a599c861
ZhouLin1992 <lzhou0922@ufl.edu>
2016-09-06 14:09:42 -0400

    219.ContainsDuplicateII
    219.1/2

---
+++ 219.ContainsDuplicateII/219.1.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+		if (nums.length <= 1 || nums == null) return false;
+		HashMap<Integer, Integer> hashmap = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			if (hashmap.containsKey(nums[i]) && i - hashmap.get(nums[i]) <= k) return true; 
+			else hashmap.put(nums[i], i);
+		}
+		return false;
+    }
+}
