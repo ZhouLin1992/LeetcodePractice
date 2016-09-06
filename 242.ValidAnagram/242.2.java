0526a4fb072e91adf0d00eaed792dae58c8cd72e
ZhouLin1992 <lzhou0922@ufl.edu>
2016-09-06 12:54:34 -0400

    242.ValidAnagram
    242.1/2

---
+++ 242.ValidAnagram/242.2.java
@@ -0,0 +1,15 @@
+public class Solution {
+	public boolean isAnagram(String s, String t) {
+		if (s.length() != t.length()) return false;
+		char[] arr1 = s.toCharArray;
+		char[] arr2 = s.toCharArray;
+
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+
+		for (int i = 0; i < s.length; i++) {
+			if (arr1[i] != arr2[i]) return false;
+		}
+		return true;
+	}
+}
