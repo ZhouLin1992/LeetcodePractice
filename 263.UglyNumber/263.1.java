f73a74ea9f35727cc16f23a8ac0f0334184ece9a
ZhouLin1992 <lzhou0922@ufl.edu>
2016-09-12 17:06:30 -0400

    263.UglyNumber
    263.1/2

---
+++ 263.UglyNumber/263.1.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean isUgly(int num) {
+		if (num <= 0) return false;
+		if (num == 1) return true;
+		while (num > 1) {
+			if (num % 2 == 0) {
+				num /= 2;
+				continue;
+			}
+			if (num % 3 == 0) {
+				num /= 3;
+				continue;
+			}
+			if (num % 5 == 0) {
+				num /= 5;
+				continue;
+			}
+			return false;
+		}
+		return true;
+    }
+}
