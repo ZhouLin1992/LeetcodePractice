c27591adc2ad5830979e3cd92d34b94e7b47791b
ZhouLin1992 <lzhou0922@ufl.edu>
2016-09-18 23:47:45 -0400

    141.LinkedListCycle
    141.1

---
+++ 141.LinkedListCycle/141.1.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+		ListNode slower = head;
+		ListNode faster = head;
+
+		while (faster != null && faster.next != null) {
+			faster = faster.next.next;
+			slower = slower.next;
+
+			if (faster == slower)
+				return true;
+		}
+		return false;
+    }
+}
