791434f7a4c6e87112546f1af026b420e9755691
ZhouLin1992 <lzhou0922@ufl.edu>
2016-10-10 15:15:42 -0400

    77.Combinations
    77.1/2

---
+++ 77.Combinations/77.1.java
@@ -0,0 +1,21 @@
+public class Solution {
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    
+    public List<List<Integer>> combine(int n, int k) {
+        combine(n, k, 1, new ArrayList<Integer>());
+        return res;
+    }
+    
+    public void combine(int n, int k, int start, ArrayList<Integer> cur) {
+        if (k > 0) {
+            for (int i = start; i <= n; i++) {
+                cur.add(i);
+                combine(n, k - 1, i + 1, cur);
+                cur.remove(cur.size() - 1);
+            }
+        } else {
+            res.add(new ArrayList<Integer>(cur));
+        }
+    }
+    
+}
