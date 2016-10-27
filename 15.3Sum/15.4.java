96c33db9378e9f687f9ab346118e652db4a00a07
ZhouLin1992 <lzhou0922@ufl.edu>
2016-10-27 15:33:37 -0400

    15.3Sum
    15.4

---
+++ 15.3Sum/15.4.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+		List<List<Integer>> res = new ArrayList<>();
+		if (nums == null || nums.length < 3)
+			return res;
+		Arrays.sort(nums);
+		for (int i = 0; i < nums.length - 2; i++) {
+			if (i > 0 && nums[i] == nums[i - 1])
+				continue;
+			int left = i + 1, right = nums.length - 1;
+			while (left < right) {
+    			int sum = nums[i] + nums[left] + nums[right];
+				if (sum == 0) {
+					List<Integer> tmp = new ArrayList<>();
+					tmp.add(nums[i]);
+					tmp.add(nums[left]);
+					tmp.add(nums[right]);
+					res.add(tmp);
+					do {
+						left++;
+					} while (left < right && nums[left] == nums[left - 1]);
+					do {
+						right--;
+					} while (left < right && nums[right] == nums[right + 1]);
+				} else if (sum < 0) {
+					do {
+						left++;
+					} while (left < right && nums[left] == nums[left - 1]);
+				} else {
+					do {
+						right--;
+					} while (left < right && nums[right] == nums[right + 1]);
+				}
+			}
+		}
+		return res;
+    }
+}
