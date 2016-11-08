944809ef2a2eae81aa888810838a879d3db46e7e
ZhouLin1992 <lzhou0922@ufl.edu>
2016-11-08 15:21:25 -0500

    173.BinarySearchTreeIterator
    173.1

---
+++ 173.BinarySearchTreeIterator/173.1.java
@@ -0,0 +1,26 @@
+public class BSTIterator {
+
+	private Deque<TreenNode> stack;
+	private TreeNode cur;
+    public BSTIterator(TreeNode root) {
+        stack = new LinkedList<>();
+        cur = root;
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return (cur != null || !stack.isEmpty());
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+  		while (cur != null) {
+  			stack.push(cur);
+  			cur = cur.left;
+  		}      
+  		cur = stack.pop();
+  		TreeNode node = cur;
+  		cur = cur.right;
+  		return node;
+    }
+}
