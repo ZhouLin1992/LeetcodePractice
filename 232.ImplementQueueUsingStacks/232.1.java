19b5401d0b24f75f809cc61a2d97edb18b655d55
ZhouLin1992 <lzhou0922@ufl.edu>
2016-12-02 19:27:34 -0500

    232.ImplementQueueUsingStacks
    232.1

---
+++ 232.ImplementQueueUsingStacks/232.1.java
@@ -0,0 +1,39 @@
+class MyQueue {
+    // input stack is for pushing elements
+    private Stack<Integer> input = new Stack<>();
+    // output stack is for poping elements
+    private Stack<Integer> output = new Stack<>();
+    
+    // Push element x to the back of the queue
+    public void push(int x) {
+        input.push(x);
+    }
+
+    // Removes the element from in front of queue
+    public void pop() {
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop());
+            }
+        }
+        output.pop();
+    }
+
+    // Get the front element
+    public int peek() {
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+
+    // Return whether the queue is empty
+    public boolean empty() {
+        if (!input.isEmpty() || !output.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+}   
