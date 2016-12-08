7d4cb7a9e8fc0be2f36b97e55d48ec32079a012a
ZhouLin1992 <lzhou0922@ufl.edu>
2016-12-08 16:23:04 -0500

    128.LongestConsecutiveSequence
    128.1

---
+++ 128.LongestConsecutiveSequence/128.1.java
@@ -0,0 +1,47 @@
+// Analysis:
+// find out the left and right bound of a subsequence
+// time complexity of putting every element into set is O(N)
+// traverse every element in the set only once, time complexity if O(N)
+
+public class Solution {
+	public int longestConsecutive(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+
+		Set<Integer> set = new HashSet<>();
+
+		// put all elements into set
+		for (int num: nums) {
+			set.add(num);
+		}
+
+		int maxLength = 0;
+		for (int num: nums) {
+			if (!set.contains(num)) {
+				continue;
+			}
+            
+            int count = 1;
+			int low = num - 1;
+			while (set.contains(low)) {
+				// 及时的移除，减少之后的查找时间
+				set.remove(low);
+				low--;
+				count++;
+			}
+
+			int high = num + 1;
+			while (set.contains(high)) {
+				set.remove(high);
+				high++;
+				count++;
+			}
+			set.remove(num);
+
+			maxLength = Math.max(maxLength, count);
+		}
+
+		return maxLength;
+	}
+}
