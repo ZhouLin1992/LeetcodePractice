5ab5d22860ffbba94ab0a2f0c4b42f4382868972
ZhouLin1992 <lzhou0922@ufl.edu>
2016-12-23 11:58:39 -0500

    235.LowestCommonAncestorOfABinarySearchTree
    235.1/2

---
+++ 235.LowestCommonAncestorOfABinarySearchTree/235.1.java
@@ -0,0 +1,27 @@
+// it is a BST
+// so left.val < root.val && right.val > root.val
+// O(N) time, O(N) space
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == null || q == null) {
+            return null;
+        }
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
