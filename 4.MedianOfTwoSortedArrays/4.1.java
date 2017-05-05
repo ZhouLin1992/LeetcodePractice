8c53cbe6f7f94a9b324d33f5a3bdf34ab8a11541
ZhouLin1992 <lzhou0922@ufl.edu>
2017-05-05 15:13:38 -0400

    4.1 Updated

--- 4.MedianOfTwoSortedArrays/4.1.java
+++ 4.MedianOfTwoSortedArrays/4.1.java
@@ -1,3 +1,34 @@
+/* Assume that the number of elements in A and B are both larger than k/2, 
+and if we compare the k/2-th smallest element in A(i.e. A[k/2-1]) and the 
+k-th smallest element in B(i.e. B[k/2 - 1]), there are three results:
+(Becasue k can be odd or even number, so we assume k is even number here 
+for simplicy. The following is also true when k is an odd number.)
+		A[k/2-1] = B[k/2-1]
+		A[k/2-1] > B[k/2-1]
+		A[k/2-1] < B[k/2-1]
+if A[k/2-1] < B[k/2-1], that means all the elements from A[0] to A[k/2-1]
+(i.e. the k/2 smallest elements in A) are in the range of k smallest elements 
+in the union of A and B. Or, in the other word, A[k/2 - 1] can never be larger 
+than the k-th smalleset element in the union of A and B.
+
+We can use a proof by contradiction. Since A[k/2 - 1] is larger than the k-th 
+smallest element in the union of A and B, then we assume it is the (k+1)-th 
+smallest one. Since it is smaller than B[k/2 - 1], then B[k/2 - 1] should be 
+at least the (k+2)-th smallest one. So there are at most (k/2-1) elements 
+smaller than A[k/2-1] in A, and at most (k/2 - 1) elements smaller than A[k/2-1] 
+in B.So the total number is k/2+k/2-2, which, no matter when k is odd or even, 
+is surly smaller than k(since A[k/2-1] is the (k+1)-th smallest element). So 
+A[k/2-1] can never larger than the k-th smallest element in the union of A and 
+B if A[k/2-1]<B[k/2-1];
+Since there is such an important conclusion, we can safely drop the first k/2 
+element in A, which are definitaly smaller than k-th element in the union of A 
+and B. This is also true for the A[k/2-1] > B[k/2-1] condition, which we should 
+drop the elements in B.
+
+时间： O(log(min(M, N)))
+*/
+
+
 public class Solution {
 	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 		int len1 = nums1.length;
@@ -22,7 +53,7 @@ public class Solution {
 			return nums1[s1 + k - 1];
 		}
 
-		// edge case 2: when k = 1
+		// edge case 2: when k = 1, 因为不可能再分了
 		if (k == 1) {
 			return Math.min(nums1[s1], nums2[s2]);
 		}
