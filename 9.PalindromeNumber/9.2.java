2fa221fe5f474c7fd9ea6b18877d935a633915f7
ZhouLin1992 <lzhou0922@ufl.edu>
2018-11-04 17:46:20 -0800

    modify 9.2

--- 9.PalindromeNumber/9.2.java
+++ 9.PalindromeNumber/9.2.java
@@ -1,6 +1,6 @@
 public class Solution {
 	public boolean isPalindrome(int x) {
-		if (x < 0 || (x != 0 && x / 10 == 0)) return false;
+		if (x < 0 || (x != 0 && x % 10 == 0)) return false;
 		int reverse = 0;
 		while (x > reverse) {
 		reverse = reverse * 10 + x % 10;
