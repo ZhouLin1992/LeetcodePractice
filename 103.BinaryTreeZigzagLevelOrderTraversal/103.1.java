e99a8d37189f3700a6bd40031421aba0f4426da8
ZhouLin1992 <lzhou0922@ufl.edu>
2019-03-09 22:43:29 -0800

    103.BinaryTreeZigzagLevelOrderTraversal

---
+++ 103.BinaryTreeZigzagLevelOrderTraversal/103.1.java
@@ -0,0 +1,45 @@
+	/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        
+        boolean fromRight = false;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+                
+                int val = curr.val;
+                if (fromRight) {
+                    list.add(0, val);
+                } else {
+                    list.add(val);
+                }
+            }
+            fromRight = !fromRight;
+            result.add(list);
+        }
+        
+        return result;
+    }
+}
