8cadf5dec38f760a333c324bd4087900cd8e842b
ZhouLin1992 <lzhou0922@ufl.edu>
2019-03-12 20:07:30 -0700

    114.FlattenBinaryTreeToLinkedList

---
+++ 114.FlattenBinaryTreeToLinkedList/114.1.java
@@ -0,0 +1,33 @@
+// recursion
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        
+        root.left = null;
+        
+        flatten(left);
+        flatten(right);
+        
+        TreeNode cur = root;
+        root.right = left;
+        while (root.right != null) {
+            root = root.right;
+        }
+        root.right = right;
+    }
+}
---
+++ 114.FlattenBinaryTreeToLinkedList/114.2.java
@@ -0,0 +1,35 @@
+// iteration
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+            if (!stack.isEmpty()) {
+                cur.right = stack.peek();
+            }
+            cur.left = null;
+        }
+    }
+}
