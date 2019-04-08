368ba102d8c9c32823349befbc06284802fe9ecb
ZhouLin1992 <lzhou0922@ufl.edu>
2019-04-07 22:17:24 -0700

    165.CompareVersionNumbers

--- 165.CompareVersionNumbers/165.2.java
+++ 165.CompareVersionNumbers/165.2.java
@@ -1,15 +1,21 @@
-public class Solution {
-    public int compareVersion(String version1, String version2) {
-		String[] str1 = version1.split("\\.");
-		String[] str2 = version2.split("\\.");
+class Solution {
+	public int compareVersion(String version1, String version2) {
+		String[] v1 = version1.split("\\.");
+		String[] v2 = version2.split("\\.");
 
-		int len = Math.max(str1.length, str2.length);
+		int len1 = v1.length;
+		int len2 = v2.length;
+		int len = Math.max(len1, len2);
 		for (int i = 0; i < len; i++) {
-			Integer num1 = i < str1.length ? Integer.parseInt(str1[i]) : 0;
-			Integer num2 = i < str2.length ? Integer.parseInt(str2[i]) : 0;
+			Integer num1 = i < len1 ? Integer.parseInt(v1[i]) : 0;
+			Integer num2 = i < len2 ? Integer.parseInt(v2[i]) : 0;
+
 			int compare = num1.compareTo(num2);
-			if (compare != 0) return compare;
+			if (compare != 0) {
+				return compare;
+			}
 		}
+
 		return 0;
-    }
+	}
 }
