d8f34a0eb91ab261a0bfe8166e3402bfe312542c
ZhouLin1992 <lzhou0922@ufl.edu>
2019-04-09 12:44:29 -0700

    198.HouseRobber

--- 198.HouseRobber/198.2.java
+++ 198.HouseRobber/198.2.java
@@ -1,13 +1,15 @@
-public class Solution {
+class Solution {
     public int rob(int[] nums) {
-		if (nums == null || nums.length == 0) return 0;
-		if (nums.length == 1) return nums[0];
-		int no = 0, yes = 0, temp;
-		for (int i = 0; i < nums.length; i++) {
-		    temp = yes;
-			yes = no + nums[i];
-			no = Math.max(no, temp);
-		}
-		return Math.max(yes, no);
+        // f(-1) = 0
+        int prev = 0;
+        // f(0) = 0
+        int curr = 0;
+        for (int num : nums) {
+            int tmp = curr;
+            curr = Math.max(prev + num, curr);
+            prev = tmp;
+        }
+        
+        return curr;
     }
 }
