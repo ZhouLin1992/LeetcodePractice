6d7ef96a3d454a273803a9b3319d5bac9f5b038a
ZhouLin1992 <lzhou0922@ufl.edu>
2019-05-29 21:04:06 -0700

    438.FindAllAnagramsInAString

---
+++ 438.FindAllAnagramsInAString/438.1.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : p.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        
+        int counter = map.size();
+        int start = 0;
+        int end = 0;
+        int len = p.length();
+        
+        while (end < s.length()) {
+            char curr = s.charAt(end);
+            if (map.containsKey(curr)) {
+                map.put(curr, map.get(curr) - 1);
+                if (map.get(curr) == 0) {
+                    counter--;
+                }
+            }
+            end++;
+            
+            while (counter == 0) {
+                char temp = s.charAt(start);
+                if (map.containsKey(temp)) {
+                    map.put(temp, map.get(temp) + 1);
+                    if (map.get(temp) > 0) {
+                        counter++;
+                    }
+                }
+                
+                if (end - start == len) {
+                    result.add(start);
+                }
+                
+                start++;
+            }
+        }
+        
+        return result;
+    }
+}
