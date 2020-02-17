5ff87d9995719c57674e99a47786ab8afb48bb7a
ZhouLin1992 <lzhou0922@ufl.edu>
2020-02-16 20:04:23 -0800

    319.BulbSwitcher

--- 319.BulbSwitcher/319.1.java
+++ 319.BulbSwitcher/319.1.java
@@ -1,4 +1,5 @@
 public class Solution {
+    // https://leetcode.com/problems/bulb-switcher/discuss/77104/Math-solution..
     public int bulbSwitch(int n) {
         return (int)Math.sqrt(n);   
     }
