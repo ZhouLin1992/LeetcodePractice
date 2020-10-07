278fcb53e5cc509ae4c234ac3e147242367b90dc
ZhouLin1992 <lzhou0922@ufl.edu>
2020-10-06 17:52:54 -0700

    274.H-Index

---
+++ 274.H-Index/274.1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        if (citations == null || citations.length == 0) {
+            return 0;
+        }
+        
+        int len = citations.length;
+        int[] counts = new int[len + 1];
+        
+        for (int c : citations) {
+            if (c > len) {
+                counts[len]++;
+            } else {
+                counts[c]++;
+            }
+        }
+        
+        int sum = 0;
+        for (int i = len; i >= 0; i--) {
+            sum += counts[i];
+            if (sum >= i) {
+                return i;
+            }
+        }
+        
+        return 0;
+    }
+}
