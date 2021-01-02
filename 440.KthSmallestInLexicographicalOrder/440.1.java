deadf9f1932df2bc390cec9b7cedec30e4b15ba7
ZhouLin1992 <lzhou0922@ufl.edu>
2021-01-02 13:11:56 -0800

    440.KthSmallestInLexicographicalOrder/

---
+++ 440.KthSmallestInLexicographicalOrder/440.1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findKthNumber(int n, int k) {
+        long cur = 1;
+        while (k > 1) {
+            long gap = findGap(cur, cur + 1, n);
+            if (gap < k) {
+                k -= gap;
+                cur += 1;
+            } else {
+                k -= 1;
+                cur *= 10;
+            }
+        }
+        return (int)cur;
+    }
+    
+    private long findGap(long a, long b, int n) {
+        long gap = 0;
+        while (a <= n) {
+            gap += Math.min(n + 1, b) - a;
+            a *= 10;
+            b *= 10;
+        }
+        return gap;
+    }
+}
